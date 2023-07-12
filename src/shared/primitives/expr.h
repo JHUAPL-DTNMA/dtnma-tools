fd5ab3018637cfe204ffbd1f2e3f5b427eaa70eb
Brian Sipos <brian.sipos@jhuapl.edu>
2023-07-12 16:50:10 -0400

    Applying license markings to source files

--- src/shared/primitives/expr.h
+++ src/shared/primitives/expr.h
@@ -1,8 +1,20 @@
-/******************************************************************************
- **                           COPYRIGHT NOTICE
- **      (c) 2013 The Johns Hopkins University Applied Physics Laboratory
- **                         All rights reserved.
- ******************************************************************************/
+/*
+ * Copyright (c) 2013-2023 The Johns Hopkins University Applied Physics
+ * Laboratory LLC.
+ *
+ * This file is part of the Delay-Tolerant Networking Management
+ * Architecture (DTNMA) Tools package.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 /*****************************************************************************
  **
  ** File Name: expr.h
