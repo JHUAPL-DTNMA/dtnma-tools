c7ef3dddd6db598385e7b39777d409be2e147308
Justin Ethier <Justin.ethier@jhuapl.edu>
2024-10-14 14:48:11 -0400

    Proposed changes for adding type checking to RPTSET parameter list values (#51)
    * Issue #38 - Address FIXME's for RPTSET
    
    Add type checking for RPTSET parameter list values.
    
    * Issue #38 - Handle type conversions for RPTSET r
    
    Allow type conversions for non-TP values passed for the RPTSET r parameter.
    
    * Issue #38 - Generalize time conversions to TD
    
    * Cleanup formatting
    
    * Issue #38 - Fixes from code review
    
    Address Brian's comments from latest code review.
    
    * Issue #38 - Fix return value for invalid floats
    
    * Issue #38 - Added test cases for TP/TD conversions
    
    * Added doxygen comments for ari_set_copy/move
    
    * Added ari_deinit calls
    
    * Issue #38 - Allow negative timespecs
    
    * Issue #38 - Do not allow TP/TD conversions
    
    * Issue #38 - Fix ARI syntax for literals

--- test/cace/test_ari_text.c
+++ test/cace/test_ari_text.c
@@ -897,10 +897,11 @@ void test_ari_text_decode_lit_typed_execset(const char *text, enum ari_prim_type
 
 TEST_CASE("ari:/RPTSET/n=null;r=725943845;", ARI_PRIM_NULL, 0)
 TEST_CASE("ari:/RPTSET/n=1234;r=725943845;(t=0;s=//test/CTRL/hi;())", ARI_PRIM_INT64, 1)
-TEST_CASE("ari:/RPTSET/n=1234;r=/TP/725943845;(t=0;s=//test/CTRL/hi;())", ARI_PRIM_INT64, 1)
-TEST_CASE("ari:/RPTSET/n=1234;r=/TP/725943845.000;(t=0;s=//test/CTRL/hi;())", ARI_PRIM_INT64, 1)
-TEST_CASE("ari:/RPTSET/n=1234;r=/TP/20230102T030405Z;(t=0;s=//test/CTRL/hi;())", ARI_PRIM_INT64, 1)
-TEST_CASE("ari:/RPTSET/n=h'6869';r=725943845;(t=0;s=//test/CTRL/hi;())(t=1;s=//test/CTRL/eh;())", ARI_PRIM_BSTR, 2)
+TEST_CASE("ari:/RPTSET/n=1234;r=725943845;(t=0.0;s=//test/CTRL/hi;())", ARI_PRIM_INT64, 1)
+TEST_CASE("ari:/RPTSET/n=1234;r=/TP/725943845;(t=/TD/0;s=//test/CTRL/hi;())", ARI_PRIM_INT64, 1)
+TEST_CASE("ari:/RPTSET/n=1234;r=/TP/725943845.000;(t=/TD/0;s=//test/CTRL/hi;())", ARI_PRIM_INT64, 1)
+TEST_CASE("ari:/RPTSET/n=1234;r=/TP/20230102T030405Z;(t=/TD/0;s=//test/CTRL/hi;())", ARI_PRIM_INT64, 1)
+TEST_CASE("ari:/RPTSET/n=h'6869';r=/TP/725943845;(t=/TD/0;s=//test/CTRL/hi;())(t=/TD/1;s=//test/CTRL/eh;())", ARI_PRIM_BSTR, 2)
 void test_ari_text_decode_lit_typed_rptset(const char *text, enum ari_prim_type_e expect_n, size_t expect_count)
 {
     ari_t ari = ARI_INIT_UNDEFINED;
@@ -1009,7 +1010,7 @@ TEST_CASE("ari:/TBL/c=1;")
 TEST_CASE("ari:/EXECSET/n=null;()")
 TEST_CASE("ari:/EXECSET/n=1234;(//test/CTRL/hi)")
 TEST_CASE("ari:/EXECSET/n=h'6869';(//test/CTRL/hi,//test/CTRL/eh)")
-TEST_CASE("ari:/RPTSET/n=1234;r=1000;(t=0;s=//test/CTRL/hi;(null,3,h'6869'))")
+TEST_CASE("ari:/RPTSET/n=1234;r=/TP/20000101T001640Z;(t=/TD/PT0S;s=//test/CTRL/hi;(null,3,h'6869'))")
 TEST_CASE("ari:/RPTSET/n=1234;r=/TP/20230102T030405Z;(t=/TD/PT0S;s=//test/CTRL/hi;(null,3,h'6869'))")
 TEST_CASE("ari://test/this/that")      // ADM path
 TEST_CASE("ari://test@1234/this/that") // ADM revision
@@ -1057,7 +1058,15 @@ TEST_CASE("ari:/ac/()", "ari:/AC/()")
 TEST_CASE("ari:/am/()", "ari:/AM/()")
 TEST_CASE("ari:/tbl/c=3;(1,2,3)", "ari:/TBL/c=3;(1,2,3)")
 TEST_CASE("ari:/execset/n=null;()", "ari:/EXECSET/n=null;()")
-TEST_CASE("ari:/rptset/n=1234;r=1000;(t=0;s=//test/ctrl/hi;(null,3,h'6869'))", "ari:/RPTSET/n=1234;r=1000;(t=0;s=//test/ctrl/hi;(null,3,h'6869'))")
+TEST_CASE("ari:/rptset/n=1234;r=/TP/1000;(t=/TD/0;s=//test/CTRL/hi;(null,3,h'6869'))", "ari:/RPTSET/n=1234;r=/TP/20000101T001640Z;(t=/TD/PT0S;s=//test/CTRL/hi;(null,3,h'6869'))")
+TEST_CASE("ari:/rptset/n=1234;r=/TP/1000;(t=0;s=//test/CTRL/hi;(null,3,h'6869'))", "ari:/RPTSET/n=1234;r=/TP/20000101T001640Z;(t=/TD/PT0S;s=//test/CTRL/hi;(null,3,h'6869'))")
+TEST_CASE("ari:/rptset/n=1234;r=/TP/1000;(t=100.5;s=//test/CTRL/hi;(null,3,h'6869'))", "ari:/RPTSET/n=1234;r=/TP/20000101T001640Z;(t=/TD/PT1M40.5S;s=//test/CTRL/hi;(null,3,h'6869'))")
+TEST_CASE("ari:/rptset/n=1234;r=1000;(t=/TD/0;s=//test/CTRL/hi;(null,3,h'6869'))", "ari:/RPTSET/n=1234;r=/TP/20000101T001640Z;(t=/TD/PT0S;s=//test/CTRL/hi;(null,3,h'6869'))")
+TEST_CASE("ari:/rptset/n=1234;r=1000.0;(t=/TD/0;s=//test/CTRL/hi;(null,3,h'6869'))", "ari:/RPTSET/n=1234;r=/TP/20000101T001640Z;(t=/TD/PT0S;s=//test/CTRL/hi;(null,3,h'6869'))")
+TEST_CASE("ari:/rptset/n=1234;r=/UVAST/1000;(t=/TD/0;s=//test/CTRL/hi;(null,3,h'6869'))", "ari:/RPTSET/n=1234;r=/TP/20000101T001640Z;(t=/TD/PT0S;s=//test/CTRL/hi;(null,3,h'6869'))")
+TEST_CASE("ari:/rptset/n=1234;r=/UVAST/0b1000;(t=/TD/0;s=//test/CTRL/hi;(null,3,h'6869'))", "ari:/RPTSET/n=1234;r=/TP/20000101T000008Z;(t=/TD/PT0S;s=//test/CTRL/hi;(null,3,h'6869'))")
+TEST_CASE("ari:/rptset/n=1234;r=/TP/1000.987654321;(t=/TD/0;s=//test/CTRL/hi;(null,3,h'6869'))", "ari:/RPTSET/n=1234;r=/TP/20000101T001640.987654321Z;(t=/TD/PT0S;s=//test/CTRL/hi;(null,3,h'6869'))")
+TEST_CASE("ari:/rptset/n=1234;r=1000.9876543210987654321;(t=/TD/0;s=//test/CTRL/hi;(null,3,h'6869'))", "ari:/RPTSET/n=1234;r=/TP/20000101T001640.987654321Z;(t=/TD/PT0S;s=//test/CTRL/hi;(null,3,h'6869'))")
 void test_ari_text_encode_decode(const char *intext, const char *expect_outtext)
 {
     ari_t    ari = ARI_INIT_UNDEFINED;
