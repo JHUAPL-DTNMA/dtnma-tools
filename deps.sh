6695cc2702c52f300c86f14b32a23a4144c0f937
Justin Ethier <justin.ethier@jhuapl.edu>
2024-12-17 13:38:16 -0500

    Add -fPIC flag for QCBOR
    Adding this to prevent linking errors when building on RHEL 9.
    This option is listed in the actual QCBOR makefile but was being overwritten by cmake during our build.

--- deps.sh
+++ deps.sh
@@ -54,6 +54,7 @@ then
   pushd ${DEPSDIR}/QCBOR
   cmake -S . -B ${BUILDDIR}/QCBOR \
     -DCMAKE_BUILD_TYPE=Debug \
+    -DCMAKE_C_FLAGS="-fPIC " \
     -DCMAKE_INSTALL_PREFIX=${DESTDIR}${PREFIX}
   cmake --build ${BUILDDIR}/QCBOR
   cmake --install ${BUILDDIR}/QCBOR
