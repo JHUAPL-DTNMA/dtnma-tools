3ea8df72ec4616707e19ad9ffa4e771aa4e7d6fb
Brian Sipos <brian.sipos@jhuapl.edu>
2025-08-04 13:52:19 -0400

    Fix API docs

--- src/cace/ari/text.h
+++ src/cace/ari/text.h
@@ -128,7 +128,7 @@ int cace_ari_text_encode(string_t text, const cace_ari_t *ari, cace_ari_text_enc
  * @param text A null-terminated UTF-8 text string.
  * @param[out] errm If non-null, this will be set to a specific error message
  * associated with any failure.
- * Regardless of the return code, if the pointed-to pointer is non-null it
+ * When the return code is non-zero, if the pointed-to pointer is non-null it
  * must be freed using ::CACE_FREE().
  * @return Zero upon success.
  */
